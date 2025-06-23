04d344a1c0632e309193ace941dbc2b136dd7471
NguyenAnhTruongTai <0306201485@caothang.edu.vn>
2025-06-23 22:00:14 +0700

    Add register Data

--- Automation.BuggyCars.Testing/obj/Debug/net8.0/Automation.BuggyCars.Testing.AssemblyInfo.cs
+++ Automation.BuggyCars.Testing/obj/Debug/net8.0/Automation.BuggyCars.Testing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Automation.BuggyCars.Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add8d46975da24879de721a90084db7c1bb5b65c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0325162c4dfa33d5eec3343545261e924addcf8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Automation.BuggyCars.Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Automation.BuggyCars.Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
